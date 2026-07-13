94326625f3810a3f20d1021bad17d9cced39cd8e
Komalpreet Kaur <randhawakomal243@gmail.com>
2026-07-12 21:48:49 -0400

    Refactored game loop for events and drawing

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,57 @@
 import sys
 import pygame
 
-if __name__ == '__main__':
-    pass
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+
+    def __init__(self) -> None:
+        pygame.init()
+
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_w, self.settings.screen_h)
+        )
+        pygame.display.set_caption(self.settings.name)
+
+        
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(
+            self.bg,
+            (self.settings.screen_w, self.settings.screen_h)
+        )
+
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+    def run_game(self) -> None:
+        self.ship = Ship(self)
+        """Main game loop."""
+        while self.running:
+            self._check_events()
+
+           
+            self._update_screen()
+
+            pygame.display.flip()
+            self.clock.tick(self.settings.FPS)
+
+    def _update_screen(self):
+        self.screen.blit(self.bg, (0, 0))
+        self.screen.blit(self.bg, (0, 0))
+        self.ship.draw()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
